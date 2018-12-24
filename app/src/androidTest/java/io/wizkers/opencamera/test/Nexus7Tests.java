9bea8cfa1c877c66095c932b3e530e3783662540
elafargue <ed@wizkers.io>
2018-12-23 19:44:04 -0800

    Super update: rename all to the io.wizkers namespace to enable running OC + Remote branch simultaneously.
    Change BLE reconnect strategy to be much more robust. Initial tests work much better.

---
+++ app/src/androidTest/java/io/wizkers/opencamera/test/Nexus7Tests.java
@@ -0,0 +1,14 @@
+package io.wizkers.opencamera.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class Nexus7Tests {
+	// Tests to run specifically on Nexus 7
+	public static Test suite() {
+		TestSuite suite = new TestSuite(MainTests.class.getName());
+		suite.addTest(TestSuite.createTest(MainActivityTest.class, "testSwitchVideo"));
+		suite.addTest(TestSuite.createTest(MainActivityTest.class, "testFocusFlashAvailability"));
+        return suite;
+    }
+}
